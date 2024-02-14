d28f92daebfa66abdec368a0a7c26f7a6387fa47
John Doe <johndoe@example.com>
2024-02-14 08:29:37 +0100

    exo 8

---
+++ src/Service/CreditsGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// Là ou la classe est déclarée (où son fichier se trouve)
+namespace App\Service;
+
+
+class CreditsGenerator
+{
+    public function getCredits(): string
+    {
+        return "An amazing application from myself. You're welcome.";
+    }
+}
